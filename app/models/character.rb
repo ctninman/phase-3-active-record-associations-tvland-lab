2c6d24e8b2700c4565f0075c49026c560ffa631a
ctninman <89664157+ctninman@users.noreply.github.com>
2022-01-24 17:27:41 -0500

    commit tree 539a8c7c5f9e690c39a2802f8ce965588452fdb0

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,27 @@
 class Character < ActiveRecord::Base
+
+  belongs_to :actor
+  belongs_to :show
+
+  #actor: should return the actor instance this character belongs to. Hint: is there an association macro you can use to create this method?
+  # def actor
+  #   self.actor
+  # end
+
+#show: should return the show instance this character belongs to. Hint: is there an association macro you can use to create this method?
+  # def show 
+
+  # end
+
+#say_that_thing_you_say: Define a method in the Character class, 
+  def say_that_thing_you_say
+    self.name + " always says: " + self.catchphrase
+
+  end
+
+#say_that_thing_you_say, using a given character's catchphrase. Using Tyrion as an example again, the returned string should look like the following:
+# tyrion.catchphrase = 'A Lannister always pays his debts'
+#   tyrion.say_that_thing_you_say
+  #=> 'Tyrion Lannister always says: A Lannister always pays his debts' 
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
   
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    all_actors = []
+    self.characters.each do |character|
+      all_actors << character.actor.first_name + " " + character.actor.last_name
+    end
+    all_actors
+  end
+
 end
